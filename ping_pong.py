9da382244d40576e6acebff4e8cc5bd435db1d7f
VariantRegginald <regginaldnwakanma@gmail.com>
2022-07-10 21:58:07 +0100

    Update ping_pong.py
    setting up ball movements and checking the top  borders

--- ping_pong.py
+++ ping_pong.py
@@ -38,6 +38,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0 , 0)
+ball.dx = 0.25
+ball.dy = 0.25
 
 
 # Game Function
@@ -73,3 +75,13 @@ wn.onkeypress(paddle_b_down , "Down")
 #Main game loop 
 while True:
     wn.update()
+
+    #Move the Ball 
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border Checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
